bca37a980b7cb130bc2b4acbe4e52dc80aa24cc1
Georgii Skorokhod <skorokhod.g@gmail.com>
2026-01-15 13:23:58 +0700

    TEMP: unlink representation rules in conjure-cp-rules

--- crates/conjure-cp-rules/src/sat/mod.rs
+++ crates/conjure-cp-rules/src/sat/mod.rs
@@ -1,4 +1,6 @@
 mod boolean;
-mod direct_int_ops;
-mod integer_repr;
-mod log_int_ops;
+
+// TODO (repr): integer representation rules
+// mod integer_repr;
+// mod direct_int_ops;
+// mod log_int_ops;
